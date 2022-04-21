fed72c2eafbf673d7ec3156fe93c57b5b94f0cb0
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-04-20 23:09:35 -0600

    Crea las funciones obtnerCliente, llenarFormulario y conectarDB
    - conectarDB conecta a la base de datos ya existente
    - obtenerCliente trae los clientes de la base de datos crm
    - llenarFormulario trae los datos del cliente al formulario

---
+++ js/editarcliente.js
@@ -0,0 +1,62 @@
+(function () {
+  let DB;
+
+  const nombreInput = document.querySelector("#nombre");
+  const emailInput = document.querySelector("#email");
+  const telefonoInput = document.querySelector("#telefono");
+  const empresaInput = document.querySelector("#empresa");
+
+  conectarDB();
+
+  document.addEventListener("DOMContentLoaded", () => {
+    // Verificar el id de la URL
+    const parametroURL = new URLSearchParams(window.location.search);
+    const idCliente = parametroURL.get("id");
+
+    if (idCliente) {
+      setTimeout(() => {
+        obtenerCliente(idCliente);
+      }, 100);
+    }
+  });
+
+  function obtenerCliente(id) {
+    const transaction = DB.transaction(["crm"], "readonly");
+    const objectStore = transaction.objectStore("crm");
+
+    const cliente = objectStore.openCursor();
+
+    cliente.onsuccess = (e) => {
+      const cursor = e.target.result;
+
+      if (cursor) {
+        if (cursor.value.id === Number(id)) {
+          llenarFormulario(cursor.value);
+        }
+
+        cursor.continue();
+      }
+    };
+  }
+
+  function llenarFormulario(datosCliente) {
+    const { nombre, email, telefono, empresa } = datosCliente;
+
+    nombreInput.value = nombre;
+    emailInput.value = email;
+    telefonoInput.value = telefono;
+    empresaInput.value = empresa;
+  }
+
+  function conectarDB() {
+    const abrirConexion = window.indexedDB.open("crm", 1);
+
+    abrirConexion.onerror = () => {
+      console.log("Hubo un error");
+    };
+
+    abrirConexion.onsuccess = () => {
+      DB = abrirConexion.result;
+    };
+  }
+})();
