f2df92d7ac4c22890651a461b817bf24bf965013
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-04-23 23:17:15 -0600

    Crea función para eliminar los clientes de la BD

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
 (function () {
   let DB;
 
+  const listadoClientes = document.querySelector("#listado-clientes");
+
   document.addEventListener("DOMContentLoaded", () => {
     crearDB();
 
     if (window.indexedDB.open(["crm"], 1)) {
       obtenerClientes();
     }
+
+    listadoClientes.addEventListener("click", eliminarRegistro);
   });
+
+  function eliminarRegistro(e) {
+    if (e.target.classList.contains("eliminar")) {
+      const idEliminar = Number(e.target.dataset.cliente);
+
+      const confirmar = confirm("¿Deseas eliminar a este cliente?");
+
+      if (confirmar) {
+        const transaction = DB.transaction(["crm"], "readwrite");
+        const objectStore = transaction.objectStore("crm");
+
+        objectStore.delete(idEliminar);
+
+        transaction.oncomplete = () => {
+          e.target.parentElement.parentElement.remove;
+        };
+        transaction.onerror = () => {};
+      }
+    }
+  }
 
   // Crea la base de datos de indexDB
   function crearDB() {
@@ -71,7 +95,7 @@
               </td>
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
               </td>
           </tr>`;
           cursor.continue();
